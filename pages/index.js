608c2c995e62c236e29e32d7b8f6d34dbeea326c
金誠俊 <kimseongjun@kim.local>
2023-08-15 22:30:40 +0900

    first-git

---
+++ pages/index.js
@@ -0,0 +1,41 @@
+// function EachPost(props) {
+//   return (
+//     <article>
+//       <a href={props.url}>
+//         <h3>{props.title}</h3>
+//       </a>
+//     </article>
+//   );
+// }
+
+// function Decoration(props) {
+//   return <div style={{ color: "red" }}>{props.children}</div>;
+// }
+
+// export default function Posts() {
+//   return (
+//     <Decoration>
+//       <h2>おすすめの記事</h2>
+//       <EachPost title="スケジュール管理と猫の理論" url="/blog/schedule" />
+//       <EachPost title="音楽が呼び起こす美味しいもの記憶" url="/blog/music" />
+//     </Decoration>
+//   );
+// }
+
+import Header from "components/header";
+import Hero from "components/hero";
+import Footer from "components/footer";
+
+export default function Home() {
+  return (
+    <>
+      <Header />
+
+      <main>
+        <Hero />
+      </main>
+
+      <Footer />
+    </>
+  );
+}
