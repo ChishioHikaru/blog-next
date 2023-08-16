f86ee048b9c293c77ceb32bc3de24c34fd5670af
kimu <tomorrowgate@kimuMacBook-Air.local>
2023-08-16 18:02:40 +0900

    last

--- components/hero.js
+++ components/hero.js
@@ -7,16 +7,16 @@ export default function Hero({ title, subtitle, imageOn = false }) {
         <h1 className={styles.title}>{title}</h1>
         <p className={styles.subtitle}>{subtitle}</p>
         {imageOn && <figure>[画像]</figure>}
-        {/* <div
-        className="link_wrap"
-        style={{ border: '1px solid #000', display: 'inline-block' }}
-      >
-        <span>通常のリンク</span>
-        <br />
-        <a href="about">ABOUT</a>
-        <br />
-        <a href="blog">BLOG</a>
-      </div> */}
+        <div
+          className="link_wrap"
+          style={{ border: '1px solid #000', display: 'inline-block' }}
+        >
+          <span>通常のリンク</span>
+          <br />
+          <a href="about">ABOUT</a>
+          <br />
+          <a href="blog">BLOG</a>
+        </div>
       </div>
     </div>
   )
